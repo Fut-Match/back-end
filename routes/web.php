cf1ec098764391468ac6cbb50c4a8a2ca875d9a2
Matheus Sandi <Matheussandi@hotmail.com>
2025-08-12 20:39:29 -0300

    feat: adicionar endpoint de verificação de integridade e documentação do Swagger

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HealthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/health', [HealthController::class, 'status']);
